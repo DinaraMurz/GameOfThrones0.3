0cc143fe5397b1ac324f5e7f3ca2ad9d9204aa03
Доллар Музыка <dollarmudic@gmail.com>
2019-05-23 18:55:05 +0600

    Добавьте файлы проекта.

---
+++ GameOfThrones/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using GameOfThrones;
+using Gecko;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Forms.Integration;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using System.Windows.Forms;
+using System.Windows.Controls;
+
+namespace GeckoWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _url;
+        private List<Person> _people;
+
+        public MainWindow()
+        {
+            _url = "https://api.got.show/api/show/characters";
+            InitializeComponent();
+
+            GetPeople();
+
+            foreach (var person in _people)
+            {
+                peopleListBox.Items.Add(person.Name);
+            }
+            
+            Gecko.Xpcom.Initialize("Firefox");
+        }
+        
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            WindowsFormsHost host = new WindowsFormsHost();
+            GeckoWebBrowser browser = new GeckoWebBrowser();
+            host.Child = browser;
+            GridWeb.Children.Add(host);
+            browser.Navigate("http://viewers-guide.hbo.com/game-of-thrones/season-6/episode-10/map/location/77/bay-of-dragons");
+        }
+        
+        public void GetPeople()
+        {
+            string json = "";
+            WebClient client = new WebClient();
+            using (Stream stream = client.OpenRead(_url))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string line = "";
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        json += line;
+                    }
+                }
+            }
+
+            _people = JsonConvert.DeserializeObject<List<Person>>(json);
+        }
+
+        private void findTextBoxDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            foreach (var item in peopleListBox.Items)
+            {
+                peopleListBox.Items.Remove(item);
+            }
+
+            foreach (var person in _people)
+            {
+                if (person.Name.Contains(findTextBox.Text) && person.Name.Length <= findTextBox.Text.Length)
+                {
+                    peopleListBox.Items.Add(person);
+                }
+            }
+        }
+
+        private void findTextBoxTextChanged(object sender, TextChangedEventArgs e)
+        {
+            peopleListBox.Items.Clear();
+
+            foreach (var person in _people)
+            {
+                if (person.Name.Contains(textBox.Text))
+                {
+                    peopleListBox.Items.Add(person);
+                }
+            }
+        }
+
+        private void peopleListBoxMouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            //System.Windows.MessageBox.Show(peopleListBox.Items[peopleListBox.SelectedIndex].ToString());
+            PersonInfo personInfo = new PersonInfo();
+            personInfo.Show();
+        }
+    }
+}
---
+++ GameOfThrones/Person.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfThrones
+{
+    public class Person
+    {
+        public List<string> Titles { get; set; }
+        public List<string> Origin { get; set; }
+        public List<string> Siblings { get; set; }
+        public List<string> Spouse { get; set; }
+        public List<string> Lovers { get; set; }
+        public List<string> Culture { get; set; }
+        public List<string> Religion { get; set; }
+        public List<string> Allegiances { get; set; }
+        public List<string> Seasons { get; set; }
+        public List<string> Appearances { get; set; }
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public string Image { get; set; }
+        public string Gender { get; set; }
+        public bool Alive { get; set; }
+        public int Death { get; set; }
+        public string Father { get; set; }
+        public string House { get; set; }
+        [JsonProperty("first_seen")]
+        public string FirstSeen { get; set; }
+        public string Actor { get; set; }
+        public string Mother { get; set; }
+        public int? Birth { get; set; }
+    }
+}
